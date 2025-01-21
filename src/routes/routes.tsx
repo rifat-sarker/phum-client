65c78242989f324e6391ce4c16a4d2dd39a38566
Rifat Sarker <rifatswd@gmail.com>
2025-01-21 14:51:48 +0600

    refactor admin routes and sidebar items for improved structure and navigation

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,7 +3,7 @@ import App from "../App";
 import About from "../pages/About";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import { adminPaths } from "./admin.routes";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
   {
     path: "/login",
