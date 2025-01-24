15007849038624b4323abf7b7bbc6ecf28fcc8f8
Rifat Sarker <rifatswd@gmail.com>
2025-01-24 09:59:29 +0600

    add academic semester management page and update sidebar for user role-based rendering

---
+++ src/pages/admin/Academic Management/AcademicSemester.tsx
@@ -0,0 +1,13 @@
+import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemester";
+
+const AcademicSemester = () => {
+    const {data} = useGetAllSemestersQuery(undefined)
+    console.log(data);
+    return (
+        <div>
+            <h1>This academic semester</h1>
+        </div>
+    );
+};
+
+export default AcademicSemester;
